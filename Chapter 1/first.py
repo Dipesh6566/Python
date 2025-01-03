31e66808e687a9f80074021fc896b284f6caa9f2
Dipesh Patel <pateldipesh26451@gmail.com>
2025-01-03 14:49:08 +0530

    Create first.py

---
+++ Chapter 1/first.py
@@ -0,0 +1,9 @@
+print(" So This is the starring of the python course")
+print( " we will be coding the codes in python")
+
+# this is a comment for a full line
+''' in python this is how we do the comments for  multiline
+ we use three single quotes to do the work'''
+''' Python have a feature of modules  which are codes done by others and we can import them in our system to use them '''
+''' we can import them using pip install name_of_module . PIP is a key word to import the module '''
+print("""this is how, we can print multiline""")
